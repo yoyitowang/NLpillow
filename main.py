b905e5505c4e431b9ff548a37a201ff50c0f763c
yoyitowang <yoyowang0720@gmail.com>
2024-01-14 16:08:57 +0800

    add schedule time

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import time
 from utils import Utils
 from goods import Goods
 from line_notify import Line
 
 if __name__ == '__main__':
     utils = Utils()
-    utils.check_goods(Goods.NLpillow.url)
-    print("Run finished")
+    while True:
+        try:
+            utils.check_goods(Goods.NLpillow.url)
+            print("Run finished")
+            time.sleep(60*6)
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            raise e
